a3aeaabf3da519bc2f2fed57178b80846435b464
Suraksha-sachan <65763236+Suraksha-sachan@users.noreply.github.com>
2020-06-02 15:59:26 +0530

    Update and rename p4.py to P4.py

--- day6/P4.py
+++ day6/P4.py
@@ -5,7 +5,7 @@ def isPerfectSquare(x):
 def isFibonacci(n): 
     return isPerfectSquare(5*n*n + 4) or isPerfectSquare(5*n*n - 4) 
      
-def ram(n):
+def suraksha(n):
   for i in range(n): 
      if (isFibonacci(i) == True): 
          print ( i,"is a Fibonacci Number")
@@ -13,4 +13,4 @@ def ram(n):
          print (i,"is a not Fibonacci Number ")
          
 num=int(input("ENTER THE RANGE: "))
-ram(num)
+suraksha(num)
