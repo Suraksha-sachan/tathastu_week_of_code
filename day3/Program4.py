1c0ae682b2362b4176270d7f1889733f3d300a7d
Suraksha-sachan <65763236+Suraksha-sachan@users.noreply.github.com>
2020-06-03 14:44:21 +0530

    Update Program4.py

--- day3/Program4.py
+++ day3/Program4.py
@@ -1,18 +1,11 @@
-
 lst = []
-
 n = int(input("Enter number of elements : ")) 
-
 for i in range(0, n):
-
     ele = int(input())
-
     lst.append(ele)
 
 print(" INPUT LIST IS : ", lst)
 
  
-
 list1 = list(dict.fromkeys(lst))
-
 print("LIST AFTER REMOVE DUPLICACY : ",list1)
