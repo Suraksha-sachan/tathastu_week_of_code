8095b22506c0ccb144c433383e263adb1cbdd0b1
Suraksha-sachan <65763236+Suraksha-sachan@users.noreply.github.com>
2020-06-03 13:56:45 +0530

    Update program2.py

--- day2/program2.py
+++ day2/program2.py
@@ -1,9 +1,7 @@
-num=int(input("ENter upto Which Series is Required:"))
+num=int(input("Enter number of term in  Series :"))
 a = 0
 b = 1
-if num < 0: 
-        print("Incorrect input") 
-else: 
+if num > 1 :
     for i in range(num):
         print(a,end=" ")
         c = a + b 
