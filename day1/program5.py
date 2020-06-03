46316a8f6492944e80dac355c6dc29b4d17a44d3
Suraksha-sachan <65763236+Suraksha-sachan@users.noreply.github.com>
2020-06-03 13:50:54 +0530

    Update program5.py

--- day1/program5.py
+++ day1/program5.py
@@ -1,24 +1,22 @@
 player1=int(input("Enter Score of player1: "))
 player2=int(input("Enter Score of player2: "))
 player3=int(input("Enter Score of player3: "))
-SR1=player1/.6
-SR2=player2/.6
-SR3=player3/.6
+SR1=(player1 * 100 ) /60
+SR2=(player2 *100)/60
+SR3=(player3*10 )/ 60
 print("StrikeRate of Player1:" , SR1)
 print("StrikeRate of Player2:" , SR2)
 print("StrikeRate of Player3:" , SR3)
 
-run1=player1*2
-run2=player2*2
-run3=player3*2
-print("Run of Player1 after Play 60 More Ball:" , run1)
-print("Run of Player2 after Play 60 More Ball:" , run2)
-print("Run of Player3 after Play 60 More Ball:" , run3)
+print("Run of Player1 after Play 60 More Ball:" , player1*2)
+print("Run of Player2 after Play 60 More Ball:" , player2*2)
+print("Run of Player3 after Play 60 More Ball:" , player3*2)
 
-six1=int(player1/6)
-six2=int(player2/6)
-six3=int(player3/6)
-print("Max Six of Player1 :" , six1)
-print("Max Six of Player2 :" , six2)
-print("Max Six of Player3 :" , six3)
+max_six1=int(player1/6)
+max_six2=int(player2/6)
+max_six3=int(player3/6)
+
+print("Max Six of Player1 :" , max_six1)
+print("Max Six of Player2 :" , max_six2)
+print("Max Six of Player3 :" , max_six3)
 
