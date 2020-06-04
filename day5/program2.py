f62ed8ef28f33974dd679ab8c9cf5db59fe3f6a3
Suraksha-sachan <65763236+Suraksha-sachan@users.noreply.github.com>
2020-06-04 08:48:12 +0530

    Update program2.py

--- day5/program2.py
+++ day5/program2.py
@@ -2,8 +2,8 @@ def replace(List):
     for i in range(size-1):
         List[i] = max(List[i+1:])
     return List
-size = int(input("Enter the size of the list: "))
+n= int(input("Enter the size of the list: "))
 List = []
-for i in range(size):
+for i in range(n):
     List.append(int(input("Enter the element number " + str(i+1) + " in the List: ")))
 print("List after replacing element with greatest element: ", replace(List))
