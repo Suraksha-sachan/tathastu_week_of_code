67a7ae38353ab4fa63935702935cad1999e504cc
Suraksha-sachan <65763236+Suraksha-sachan@users.noreply.github.com>
2020-06-04 08:39:28 +0530

    Update program3.py

--- day5/program3.py
+++ day5/program3.py
@@ -10,15 +10,12 @@ def maximum(houseval, n):
               if n == 2:
                            return value2
 
-
               max_val = None
               for i in range(2, n):
                            max_val = max(houseval[i]+value1, value2)
                            value1 = value2
                            value2 = max_val
               return max_val
-
- 
 
 houseval = [6, 7, 1, 3, 8, 2, 4]
 n = len(houseval)
