feb0d67489f236f6706723e590ddf12350d2accc
Suraksha-sachan <65763236+Suraksha-sachan@users.noreply.github.com>
2020-06-04 08:03:25 +0530

    Update Program2.py

--- day4/Program2.py
+++ day4/Program2.py
@@ -1,22 +1,13 @@
 def Sort(list):
-
     length = len(list)
-
     for i in range(0, length):
-
         for j in range(0, length-i-1):
-
             if (list[j][1] > list[j + 1][1]):
-
                 temp = list[j]
-
                 list[j]= list[j + 1]
-
                 list[j + 1]= temp
-
     return list
 
 
 list =[['a', 10], ['b', 5], ['c', 20], ['d', 15]]
-
 print(Sort(list))
